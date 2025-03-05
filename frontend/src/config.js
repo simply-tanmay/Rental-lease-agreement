a088541f15b6822bb2d9b218f7e3a61925507f9a
simply-tanmay <trawal604@gmail.com>
2025-03-06 00:03:26 +0530

    made some local variables for contract address changing. Practicing encapsulation B)

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+// src/config.js
+const config = {
+    contractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    // You can add more variables as needed
+  };
+  
+  export default config;
+  
