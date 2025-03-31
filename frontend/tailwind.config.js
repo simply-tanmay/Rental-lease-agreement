cdaf34fa8f1564577838f0c5ba2cda345cec5ef4
simply-tanmay <trawal604@gmail.com>
2025-04-01 02:30:15 +0530

    Tailwind Configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#10B981',
+        background: '#F3F4F6',
+      }
+    }
+  }
 }
-
